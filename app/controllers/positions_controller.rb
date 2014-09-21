fbdb45107123075a095863791794a11c4a7e6b0e
Lars <github@lar5.com>
2014-09-20 22:17:39 -0700

    Corner swap filter.

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,10 +1,19 @@
 class PositionsController < ApplicationController
+  CP_ENUM = Position.corner_swaps
+
   def index
     BigThought.populate_db params[:regenerate]
 
     @query = {}
+
+    @pcp = params[:cp]
+    @query['corner_swap'] = CP_ENUM['no'] if @pcp == 'None'
+    @query['corner_swap'] = CP_ENUM['diagonal'] if @pcp == 'Diagonal'
+    @query['corner_swap'] = [CP_ENUM['left'], CP_ENUM['right'], CP_ENUM['front'], CP_ENUM['back']] if @pcp == 'Adjacent'
+
     @query['oriented_corners'] = params[:co].to_i if params[:co].present?
     @query['oriented_edges'] = params[:eo].to_i if params[:eo].present?
+
     @positions = Position.includes(:best_alg).where(@query).order(:ll_code)
   end
 
