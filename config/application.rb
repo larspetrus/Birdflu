dd5ad241902279ee5f6aa0259ab424f7532d52fc
Lars <github@lar5.com>
2015-07-31 20:34:49 -0700

    Fragment caching SVG icons.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Algdb
   class Application < Rails::Application
+
+    config.cache_store = :memory_store
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
