df7d501e83650e4c92b7d67bd15b351e4dc7f6c3
Lars <github@lar5.com>
2015-10-28 17:13:18 -0700

    Roofpigs now have correct titles.

---
+++ app/assets/javascripts/common_code.js
@@ -0,0 +1,16 @@
+function roofpig_dialog(title, alg, u_setup) {
+  CubeAnimation.create_in_dom('#show-alg', 'alg='+alg+'|base=AD|flags=showalg'+u_setup, "class='roofpig rp160'");
+  $('#show-alg').dialog({
+    width: '200px',
+    title: title,
+    modal: true,
+    closeOnEscape: true,
+    dialogClass: 'lars-dialog',
+    close: function( event, ui ) {
+      var cube_id = $('#show-alg').children().attr('data-cube-id');
+      if (cube_id) {
+        CubeAnimation.by_id[cube_id].remove();
+      }
+    }
+  });
+}
