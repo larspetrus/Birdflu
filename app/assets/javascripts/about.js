06bb3abfc5f43638089d6b8cb1b2afdb14ce33d8
Lars <github@lar5.com>
2015-11-26 09:03:18 -0800

    Added About... dialog

---
+++ app/assets/javascripts/about.js
@@ -0,0 +1,10 @@
+$(document).on('click', '#about-link', function(e) {
+  $('#about-box').dialog({
+    position: { my: 'center top', at: 'center bottom', of: '#pickers' },
+    width: '240px',
+    title: 'About Algopalooza',
+    modal: true,
+    closeOnEscape: true,
+    dialogClass: 'lars-dialog'
+  });
+})
