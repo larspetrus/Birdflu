7bdbf1acc55cf71e0424329ee1a539c64693e03c
Lars <github@lar5.com>
2016-07-21 17:40:24 -0700

    Yay! Three half decent alg sets!

--- app/models/raw_alg.rb
+++ app/models/raw_alg.rb
@@ -13,6 +13,8 @@ class RawAlg < ActiveRecord::Base
     set_speed
   end
 
+  NOTHING_ID = 1
+
   def self.make(alg, length = 1)
     std_alg = Algs.display_variant(alg)
     RawAlg.create(_moves: Algs.pack(std_alg), u_setup: Algs.standard_u_setup(std_alg), length: length)
