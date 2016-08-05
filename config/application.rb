c773122f1eb5575449909d0d31db910ea1922650
Lars <github@lar5.com>
2016-08-04 23:19:00 -0700

    Project rename

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Algdb
+module Birdflu
   class Application < Rails::Application
 
     config.cache_store = :memory_store
