4dfe16703710f7ec118449fda89745875a04cf47
Lars <github@lar5.com>
2016-08-19 16:15:35 -0700

    Starting on left nav UI

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -2,7 +2,7 @@ $(document).on('click', '#about-link', function(event) {
   $('#about-box').dialog({
     position: { my: 'center top', at: 'center bottom', of: '#pickers' },
     width: '240px',
-    title: 'About Algopalooza',
+    title: 'What is this?',
     modal: true,
     closeOnEscape: true,
     dialogClass: 'lars-dialog'
