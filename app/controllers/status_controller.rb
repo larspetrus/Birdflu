ad7d1def9e64630a397732c4d8dd114ad516f6e5
Lars <github@lar5.com>
2016-08-20 12:23:23 -0700

    Cache expensive status queries

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -3,5 +3,7 @@
 class StatusController < ApplicationController
   def index
     @ruby_version = "#{RUBY_VERSION} patch #{RUBY_PATCHLEVEL} platform #{RUBY_PLATFORM}"
+    @raw_alg_count ||= @@raw_alg_count ||= RawAlg.count
+    @combo_alg_count ||= @@combo_alg_count ||= ComboAlg.count
   end
 end
