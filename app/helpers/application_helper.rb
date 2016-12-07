d1cf98aab938573f34938c7fcaa0625f555d3348
Lars <github@lar5.com>
2016-12-06 20:37:10 -0800

    Setup alg sectionalized for easier reading.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,15 @@
 
 module ApplicationHelper
   def spaced_number(count)
-    number_with_delimiter(count, delimiter: "\u2009")
+    number_with_delimiter(count, delimiter: "\u2009") # unicode 2009 is "thin space"
+  end
+
+  def alg_sections(alg)
+    sections = alg.split(' ').each_slice(4).to_a.map{|slice| slice.join(' ')}
+    sections.map.with_index { |slice, i| hlp.content_tag(:span, slice, class: (i.odd? ? :odd : :even)) }
+  end
+
+  def hlp
+    ActionController::Base.helpers
   end
 end
