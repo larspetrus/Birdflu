30ec794eae90261b786e2fab6fb328d9a23fe301
Lars <github@lar5.com>
2017-01-02 20:47:32 -0800

    More tests!

--- app/helpers/alg_sets_helper.rb
+++ app/helpers/alg_sets_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module AlgSetsHelper
   def avg_format(avg)
     return '--' unless avg
---
+++ spec/helpers/alg_sets_helper_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe AlgSetsHelper do
+  let(:uncalculated_as) {AlgSet.make(algs: '', name: 'no').computing_off}
+
+  it 'fmt_coverage_percent' do
+    expect(helper.fmt_coverage_percent(uncalculated_as)).to eq('--')
+    expect(helper.fmt_coverage_percent(double(coverage: 22, subset_pos_ids: 1..50.to_i))).to eq('44%')
+  end
+
+  it 'fmt_coverage_fraction' do
+    expect(helper.fmt_coverage_fraction(uncalculated_as, false)).to eq('')
+    expect(helper.fmt_coverage_fraction(uncalculated_as, true)).to eq('')
+
+    half_covered = double(full_coverage?: false, coverage: 22, subset_pos_ids: 1..50.to_i)
+    expect(helper.fmt_coverage_fraction(half_covered, false)).to eq('22/50')
+    expect(helper.fmt_coverage_fraction(half_covered, true)).to eq('(22/50)')
+  end
+end
