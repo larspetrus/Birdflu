74c8ca455c03cc4c4e65602692a57dfeeb4c3425
Lars <github@lar5.com>
2017-01-24 12:24:02 -0800

    Rephrasing. "Favorite" can be a a verb in 2017!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   around_action :keep_track
 
   MAIN_NAME = 'Main'
-  FAV_NAME = 'Alg stars'
+  FAV_NAME = 'Favorites'
   ALGSETS_NAME = 'Combos'
   FMC_NAME = 'FMC'
 
