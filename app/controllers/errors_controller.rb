3b57d2e75afab0fd9f0c50fd429ecbbfc8f9b790
Lars <github@lar5.com>
2017-06-24 14:00:45 -0700

    Simple 404 & 500 pages.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,13 +1,13 @@
 class ErrorsController < ApplicationController
   def not_found
     @request_url = request&.original_url
-    logger.error("Showing 404 page for '#{@request_url}'")
+    logger.warn("Showing 404 page for '#{@request_url}'")
     render(:status => 404)
   end
 
   def internal_server_error
     @request_url = request&.original_url
-    logger.error("Showing 500 page for '#{@request_url}'")
+    logger.warn("Showing 500 page for '#{@request_url}'")
     render(:status => 500)
   end
 end
